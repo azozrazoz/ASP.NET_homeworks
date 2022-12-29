aabf05923a54771b5925d0876eb0b963101f53cd
azozrazoz <doshan.de@gmail.com>
2022-12-29 19:47:00 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            var ip = requestContext.HttpContext.Request.UserHostAddress;
+            var response = requestContext.HttpContext.Response;
+
+            response.Write("<h3>My IP address: " + ip + "</h3>");
+
+            // Console.WriteLine(ip.ToString());
+        }
+    }
+}
