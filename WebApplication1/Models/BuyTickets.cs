1396237339022db1b4a1e2bc3bb7e0e572923931
azozrazoz <doshan.de@gmail.com>
2022-12-29 20:04:10 +0600

    add model Movie BuyTickets

---
+++ WebApplication1/Models/BuyTickets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class BuyTickets
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Person { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string CreateBy { get; set; }
+    }
+}
