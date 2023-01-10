19cd3388df03fbc504d9305946bb7906ac69c6a8
azozrazoz <doshan.de@gmail.com>
2023-01-10 19:21:03 +0600

    add Register funcs

--- WebApplication1/Controllers/RegisterController.cs
+++ WebApplication1/Controllers/RegisterController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class RegisterController : Controller
     {
+        AccountContext db = new AccountContext();
         // GET: Register
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register(int id)
+        {
+            ViewBag.Id = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public string Register(Account account)
+        {
+            account.FirstName = "Alex";
+            account.LastName = "";
+            account.Email = "alex.a@gmail.com";
+            account.Gender = true;
+            account.CreatedDate = DateTime.Now;
+            db.Accounts.Add(account);
+            db.SaveChanges();
+
+
+            return $"{account.FirstName}, welcome!";
         }
     }
 }
--- WebApplication1/Models/Account.cs
+++ WebApplication1/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -15,9 +16,10 @@ namespace WebApplication1.Models
 
         public string Email { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        public string Gender { get; set; }
+        public bool Gender { get; set; }
 
         public DateTime CreatedDate { get; set; }
     }
